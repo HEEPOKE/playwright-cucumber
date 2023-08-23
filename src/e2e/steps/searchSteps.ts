efc75ef6f4d853910fd027ccdb41d1a2e298fd84
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2023-08-23 13:45:01 +0700

    :money_with_wings: refactor wrapper search

--- src/e2e/steps/searchSteps.ts
+++ src/e2e/steps/searchSteps.ts
@@ -1,35 +1,33 @@
 import { Given, When, Then, After } from "@cucumber/cucumber";
-import { Browser, Page, expect, chromium } from "@playwright/test";
+import { Browser, expect, chromium } from "@playwright/test";
 import config from "../../configs/config";
 import pageFixture from "../../hooks/fixture";
+import SearchPage from "../../pages/serachPage";
 
 let browser: Browser;
-let page: Page;
+let page: SearchPage;
 
 Given("I open the YouTube website", async () => {
   browser = await chromium.launch({ headless: false });
-  page = await browser.newPage();
-  await page.goto(config.BASE_URL);
-  pageFixture.logger.info("Navigation to Youtube")
+  const playwrightPage = await browser.newPage();
+  page = new SearchPage(playwrightPage);
+  await page.goTo(config.BASE_URL)
+  pageFixture.logger.info("Navigation to Youtube");
 });
 
 When("I search for {string}", async (searchQuery: string) => {
-  await page.fill('input[id="search"]', searchQuery);
-  await page.press('input[id="search"]', "Enter");
-  await page.waitForSelector("#search-results");
-  pageFixture.logger.info(`Search song name is ${searchQuery}`)
+  await page.searchForVideo(searchQuery);
+  pageFixture.logger.info(`Search song name is ${searchQuery}`);
 });
 
 Then("I should see search results", async () => {
-  const searchResults = await page.$$("#search-results .search-result");
-  expect(searchResults.length).toBeGreaterThan(0);
+  const searchResultsCount = await page.getSearchResultsCount();
+  expect(searchResultsCount).toBeGreaterThan(0);
 });
 
 Then("I play the video {string}", async (videoTitle: string) => {
-  const videoSelector = `//yt-formatted-string[contains(text(), "${videoTitle}")]`;
-  await page.click(videoSelector);
-  await page.waitForSelector("video");
-  pageFixture.logger.info("Play Song on Youtube")
+  await page.clickVideo(videoTitle);
+  pageFixture.logger.info("Play Song on Youtube");
 });
 
 After(async () => {
--- src/helpers/wrapper/PlaywrightWrappers.ts
+++ src/helpers/wrapper/PlaywrightWrappers.ts
@@ -16,6 +16,18 @@ class PlaywrightWrapper {
   async navigateTo(link: string) {
     await Promise.all([this.page.waitForNavigation(), this.page.click(link)]);
   }
+
+  async fillInput(locator: string, value: string) {
+    const inputElement = this.page.locator(locator);
+    await inputElement.waitFor({ state: 'visible' });
+    await inputElement.fill(value);
+  }
+
+  async getText(locator: string): Promise<string> {
+    const element = this.page.locator(locator);
+    await element.waitFor({ state: 'visible' });
+    return await element.textContent();
+  }
 }
 
 export default PlaywrightWrapper;
